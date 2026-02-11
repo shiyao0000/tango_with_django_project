c62a558cd2b45da4e45c67c94aed9154b60d22a9
shiyao zhang <2881854z@student.gla.ac.uk>
2026-02-11 09:44:00 +0800

    fifth commit

--- rango/forms.py
+++ rango/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from rango.models import Page,Category
 
- class CategoryForm(forms.ModelForm):
+class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -15,6 +15,16 @@ class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
 
     class Meta:
         model = Page
