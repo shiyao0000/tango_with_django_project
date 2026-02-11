3505d5092a164805da100354709e34cbcdf8ce1a
shiyao zhang <2881854z@student.gla.ac.uk>
2026-02-11 09:16:19 +0800

    forth commot

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    
 admin.site.register(Category)
 admin.site.register(Page)
--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category,Page
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
-    
+def about(request):
+    context_dict = {}
+    context_dict['message'] = "This is Rango's about page."
+    return render(request, 'rango/about.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
